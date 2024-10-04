beadeead09560ca535ec6c29d44d8ee0e1ca6f55
Ilya Tyagin <tyagin@udel.edu>
2024-10-04 15:47:49 -0400

    Implemented lazy JSON abstract loader

--- util/text_util.py
+++ util/text_util.py
@@ -1,16 +1,18 @@
-def get_abstr_text(abstr_id, sents_db) -> str:
-    cur_text = ''
-    for i in range(100):
-        cur_sent_id = f's:{abstr_id}:1:{i}'
-        #print(cur_sent_id)
-        if cur_sent_id in sents_db:
-            cur_text += (
-              sents_db[cur_sent_id]['sent_text']
-            )
-        else:
-            break
-    return cur_text
+# def get_abstr_text(abstr_id, sents_db) -> str:
+#     cur_text = ''
+#     for i in range(100):
+#         cur_sent_id = f's:{abstr_id}:1:{i}'
+#         #print(cur_sent_id)
+#         if cur_sent_id in sents_db:
+#             cur_text += (
+#               sents_db[cur_sent_id]['sent_text']
+#             )
+#         else:
+#             break
+#     return cur_text
 
+def get_abstr_text(abstr_id, abstr_db) -> str:
+    return abstr_db[abstr_id]
 
 def get_path_context(
     edge_to_pmid_dict,
